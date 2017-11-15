f226d6994f0ab8b2910628b8963ecb17d2111f49
Hendra Anggrian <hendraanggrian@gmail.com>
2017-11-15 16:49:38 +0700

    0.7, since version 0.6 is royally fucked

--- bundler-annotations/src/main/java/com/hendraanggrian/bundler/Extra.java
+++ bundler-annotations/src/main/java/com/hendraanggrian/bundler/Extra.java
@@ -13,8 +13,8 @@ import static java.lang.annotation.RetentionPolicy.CLASS;
  * {@literal @}Extra("EXTRA_COUNTRY") String country;
  * </code></pre>
  */
-@Target(FIELD)
 @Retention(CLASS)
+@Target(FIELD)
 public @interface Extra {
 
     String SUFFIX = "_ExtraBinding";
--- bundler-annotations/src/main/java/com/hendraanggrian/bundler/State.java
+++ bundler-annotations/src/main/java/com/hendraanggrian/bundler/State.java
@@ -6,8 +6,15 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
-@Target(FIELD)
+/**
+ * Bind state to field with this annotation.
+ * Field cannot be private as it would be inaccessible to binding class.
+ * <pre><code>
+ * {@literal @}State("EXTRA_COUNTRY") String country;
+ * </code></pre>
+ */
 @Retention(CLASS)
+@Target(FIELD)
 public @interface State {
 
     String SUFFIX = "_StateBinding";
